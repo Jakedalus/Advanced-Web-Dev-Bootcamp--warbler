e60e4fcbda412a19c62d43963a22d7381eb2f56a
Jakedalus <jacquedalus@gmail.com>
2019-08-05 16:48:55 -0400

    Add errorHandler

--- warbler/warbler-server/index.js
+++ warbler/warbler-server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const errorHandler = require('./handlers/error');
+
 const PORT = 8081;
 
 app.use(cors());
@@ -14,6 +16,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(errorHandler);
 
 
 app.listen(PORT, function() {
