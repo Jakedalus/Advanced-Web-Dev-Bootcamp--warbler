bd56b2548dce5491344a87b794659d9188266ba5
Jakedalus <jacquedalus@gmail.com>
2019-08-08 15:29:41 -0400

    Add signin route and handlers

--- warbler/warbler-server/routes/auth.js
+++ warbler/warbler-server/routes/auth.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../handlers/auth');
-
-console.log('signup:', signup);
+const { signup, signin } = require('../handlers/auth');
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 module.exports = router;
